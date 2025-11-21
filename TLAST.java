b20042b0cd6d76549f7edb956da304a9c3b965a8
gabrielglenpakpahan-sys <gabrielglenpakpahan@gmail.com>
2025-11-21 09:43:06 +0700

    PAIR 17
    SELESAI

--- TLAST.java
+++ TLAST.java
@@ -1,6 +1,7 @@
 //Gebriel Glen Marcel Pakpahan - 12S25019//
 //Maria Octavia Simorangkir - 12S25033//
 
+
 import java.util.*;
 import java.lang.Math;
 
@@ -8,156 +9,132 @@ public class TLAST {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String[] deskripsitugas = new String[10], kodematkul = new String[10], dosen = new String[10], deadline = new String[10], nimnama = new String[10], status = new String[10], matkul = new String[10], teks = new String[10];
-        double[] tingkatkesulitan = new double[10], prioritas = new double[10];
-        int[] harihinggadeadline = new int[10];
-        String task;
-        int i, n;
+        String[] descriptions = new String[10], codes = new String[10], courses = new String[10], lecturers = new String[10], deadlines = new String[10], filenames = new String[10], statuses = new String[10];
+        int[] difficulties = new int[10], days = new int[10];
+        double[] priorities = new double[10];
+        int taskCount;
 
-        n = 0;
-        inisialisasi(deskripsitugas, kodematkul, matkul, dosen, deadline, nimnama, tingkatkesulitan, harihinggadeadline, status, prioritas, teks);
-        for (i = 0; i <= 9; i++) {
-            task = input.nextLine();
-            if (task.equals("Add task")) {
-                n = addtask(deskripsitugas, kodematkul, matkul, dosen, deadline, nimnama, tingkatkesulitan, harihinggadeadline, status, prioritas, teks, i, n);
-            } else {
-                if (task.equals("Update task status")) {
-                    updatetaskstatus(kodematkul, status, n);
-                    i = i - 1;
-                } else {
-                    if (task.equals("Show assigment")) {
-                        i = i - 1;
-                    } else {
-                        if (task.equals("---")) {
-                            i = 10;
-                        }
-                    }
+        taskCount = 0;
+        String command;
+
+        command = "";
+        while (!command.equals("---")) {
+            command = input.nextLine();
+            if (command.equals("Add task")) {
+                if (taskCount < 10) {
+                    addTask(descriptions, codes, courses, lecturers, deadlines, filenames, difficulties, days, statuses, priorities, taskCount);
                 }
             }
+            if (command.equals("Update task status")) {
+                updateTask(codes, statuses, taskCount);
+            }
+            if (command.equals("Show assigment")) {
+                showAssignments(descriptions, codes, courses, lecturers, deadlines, filenames, difficulties, days, statuses, priorities, taskCount);
+            }
         }
-        descending(deskripsitugas, kodematkul, matkul, dosen, deadline, nimnama, tingkatkesulitan, harihinggadeadline, status, prioritas, teks, i, n);
-        output(deskripsitugas, kodematkul, matkul, dosen, deadline, nimnama, status, prioritas, teks, n);
     }
     
-    public static int addtask(String[] deskripsitugas, String[] kodematkul, String[] matkul, String[] dosen, String[] deadline, String[] nimnama, double[] tingkatkesulitan, int[] harihinggadeadline, String[] status, double[] prioritas, String[] teks, int i, int n) {
-        deskripsitugas[i] = input.nextLine();
-        kodematkul[i] = input.nextLine();
-        matkul[i] = input.nextLine();
-        dosen[i] = input.nextLine();
-        deadline[i] = input.nextLine();
-        nimnama[i] = input.nextLine();
-        tingkatkesulitan[i] = Double.parseDouble(input.nextLine());
-        harihinggadeadline[i] = Integer.parseInt(input.nextLine());
-        status[i] = input.nextLine();
-        prioritas[i] = tingkatkesulitan[i] * 1.0 / harihinggadeadline[i];
-        if (prioritas[i] > 3) {
-            teks[i] = "Penting! Anda harus mengerjakan tugas ini segera";
-        } else {
-            if (prioritas[i] <= 3 && prioritas[i] >= 1.5) {
-                teks[i] = "Tugas ini memiliki prioritas menengah";
-            } else {
-                if (prioritas[i] < 1.5) {
-                    teks[i] = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
-                } else {
-                    teks[i] = "---";
-                }
-            }
-        }
-        n = n + 1;
+    public static void addTask(String[] descriptions, String[] codes, String[] courses, String[] lecturers, String[] deadlines, String[] filenames, int[] difficulties, int[] days, String[] statuses, double[] priorities, int count) {
+        descriptions[count] = input.nextLine();
+        codes[count] = input.nextLine();
+        courses[count] = input.nextLine();
+        lecturers[count] = input.nextLine();
+        deadlines[count] = input.nextLine();
+        filenames[count] = input.nextLine();
+        difficulties[count] = Integer.parseInt(input.nextLine());
+        days[count] = Integer.parseInt(input.nextLine());
+        statuses[count] = input.nextLine();
+        priorities[count] = calculatePriority(difficulties[count], days[count]);
+        count = count + 1;
+    }
+    
+    public static double calculatePriority(int difficulty, int days) {
+        double priority;
+
+        priority = difficulty * 1.0 / days;
         
-        return n;
+        return priority;
     }
     
-    public static void descending(String[] deskripsitugas, String[] kodematkul, String[] matkul, String[] dosen, String[] deadline, String[] nimnama, double[] tingkatkesulitan, int[] harihinggadeadline, String[] status, double[] prioritas, String[] teks, int i, int n) {
-        int h;
-        String svdeskripsitugas, svkodematkul, svdosen, svdeadline, svnimnama, svstatus, svmatkul, svteks;
-        double svtingkatkesulitan, svprioritas;
-        int svharihinggadeadline;
+    public static String getRecommendation(double priority) {
+        String recommendation;
 
-        for (i = 0; i <= n - 1; i++) {
-            for (h = i + 1; h <= n - 1; h++) {
-                if (prioritas[i] < prioritas[h]) {
-                    svdeskripsitugas = deskripsitugas[h];
-                    deskripsitugas[h] = deskripsitugas[i];
-                    deskripsitugas[i] = svdeskripsitugas;
-                    svkodematkul = kodematkul[h];
-                    kodematkul[h] = kodematkul[i];
-                    kodematkul[i] = svkodematkul;
-                    svmatkul = matkul[h];
-                    matkul[h] = matkul[i];
-                    matkul[i] = svmatkul;
-                    svdosen = dosen[h];
-                    dosen[h] = dosen[i];
-                    dosen[i] = svdosen;
-                    svdeadline = deadline[h];
-                    deadline[h] = deadline[i];
-                    deadline[i] = svdeadline;
-                    svnimnama = nimnama[h];
-                    nimnama[h] = nimnama[i];
-                    nimnama[i] = svnimnama;
-                    svtingkatkesulitan = tingkatkesulitan[h];
-                    tingkatkesulitan[h] = tingkatkesulitan[i];
-                    tingkatkesulitan[i] = svtingkatkesulitan;
-                    svharihinggadeadline = harihinggadeadline[h];
-                    harihinggadeadline[h] = harihinggadeadline[i];
-                    harihinggadeadline[i] = svharihinggadeadline;
-                    svstatus = status[h];
-                    status[h] = status[i];
-                    status[i] = svstatus;
-                    svprioritas = prioritas[h];
-                    prioritas[h] = prioritas[i];
-                    prioritas[i] = svprioritas;
-                    svteks = teks[h];
-                    teks[h] = teks[i];
-                    teks[i] = svteks;
+        if (priority > 3) {
+            recommendation = "Penting! Anda harus mengerjakan tugas ini segera";
+        } else {
+            if (priority >= 1.5) {
+                recommendation = "Tugas ini memiliki prioritas menengah";
+            } else {
+                recommendation = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
+            }
+        }
+        
+        return recommendation;
+    }
+    
+    public static void showAssignments(String[] descriptions, String[] codes, int[] courses, int[] lecturers, int[] deadlines, int[] filenames, int[] statuses, int[] difficulties, int[] days, int[] priorities, int count) {
+        int i, j;
+        String outputString, recText, tempString;
+        int tempInt;
+        double tempReal;
+
+        for (i = 0; i <= count - 1; i++) {
+            for (j = 0; j <= count - 1; j++) {
+                if (priorities[j] < priorities[j + 1]) {
+                    tempReal = priorities[j];
+                    priorities[j] = priorities[j + 1];
+                    priorities[j + 1] = tempReal;
+                    tempString = descriptions[j];
+                    descriptions[j] = descriptions[j + 1];
+                    descriptions[j + 1] = tempString;
+                    tempString = codes[j];
+                    codes[j] = codes[j + 1];
+                    codes[j + 1] = tempString;
+                    tempString = courses[j];
+                    courses[j] = courses[j + 1];
+                    courses[j + 1] = tempString;
+                    tempString = lecturers[j];
+                    lecturers[j] = lecturers[j + 1];
+                    lecturers[j + 1] = tempString;
+                    tempString = deadlines[j];
+                    deadlines[j] = deadlines[j + 1];
+                    deadlines[j + 1] = tempString;
+                    tempString = filenames[j];
+                    filenames[j] = filenames[j + 1];
+                    filenames[j + 1] = tempString;
+                    tempString = statuses[j];
+                    statuses[j] = statuses[j + 1];
+                    statuses[j + 1] = tempString;
+                    tempInt = difficulties[j];
+                    difficulties[j] = difficulties[j + 1];
+                    difficulties[j + 1] = tempInt;
+                    tempInt = days[j];
+                    days[j] = days[j + 1];
+                    days[j + 1] = tempInt;
                 }
             }
         }
-    }
-    
-    public static void inisialisasi(String[] deskripsitugas, String[] kodematkul, String[] matkul, String[] dosen, String[] deadline, String[] nimnama, double[] tingkatkesulitan, int[] harihinggadeadline, String[] status, double[] prioritas, String[] teks) {
-        int a;
-
-        for (a = 0; a <= 9; a++) {
-            deskripsitugas[a] = "";
-            kodematkul[a] = "";
-            matkul[a] = "";
-            dosen[a] = "";
-            deadline[a] = "";
-            nimnama[a] = "";
-            tingkatkesulitan[a] = 0;
-            harihinggadeadline[a] = 0;
-            status[a] = "";
-            prioritas[a] = 0;
-            teks[a] = "";
+        for (i = 0; i <= count - 1; i++) {
+            System.out.println("Prioritas: " + toFixed(priorities[i],2));
+            if (statuses[i] == "Selesai") {
+                outputString = descriptions[i] + "|" + codes[i] + "|" + courses[i] + "|" + lecturers[i] + "|" + filenames[i] + "|" + statuses[i];
+            } else {
+                recText = getRecommendation(priorities[i]);
+                outputString = descriptions[i] + "|" + codes[i] + "|" + courses[i] + "|" + lecturers[i] + "|" + deadlines[i] + "|" + filenames[i] + "|" + statuses[i] + "|" + recText;
+            }
         }
+        System.out.println(outputString);
     }
     
-    public static void output(String[] deskripsitugas, String[] kodematkul, String[] matkul, String[] dosen, String[] deadline, String[] nimnama, String[] status, double[] prioritas, String[] teks, int n) {
+    public static void updateTask(String[] codes, String[] statuses, int count) {
+        String targetCode, newStatus;
         int i;
 
-        for (i = 0; i <= n - 1; i++) {
-            if (status[i].equals("Selesai")) {
-                System.out.println("Prioritas: " + toFixed(prioritas[i],2));
-                System.out.println(deskripsitugas[i] + "|" + kodematkul[i] + "|" + matkul[i] + "|" + dosen[i] + "|" + nimnama[i] + "|" + status[i]);
-            } else {
-                if (status[i].equals("Belum Selesai")) {
-                    System.out.println("Prioritas: " + toFixed(prioritas[i],2));
-                    System.out.println(deskripsitugas[i] + "|" + kodematkul[i] + "|" + matkul[i] + "|" + dosen[i] + "|" + deadline[i] + "|" + nimnama[i] + "|" + status[i] + "|" + teks[i]);
-                }
-            }
-        }
-    }
-    
-    public static void updatetaskstatus(String[] kodematkul, String[] status, int n) {
-        String kodenya, setatus;
-        int h;
-
-        kodenya = input.nextLine();
-        for (h = 0; h <= n - 1; h++) {
-            if (kodenya.equals(kodematkul[h])) {
-                setatus = input.nextLine();
-                status[h] = setatus;
+        targetCode = input.nextLine();
+        newStatus = input.nextLine();
+        for (i = 0; i <= count - 1; i++) {
+            if (codes[i].equals(targetCode)) {
+                statuses[i] = newStatus;
             }
         }
     }
